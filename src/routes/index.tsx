fa230a5e2f5b8099dabd642136062c55c9a50d1f
Ajiths10 <ajithsnair10@gmail.com>
2023-05-05 20:57:17 +0530

    home component added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Ckeditor from "../component/Ckeditor";
 import AdvancedEditor from "../component/AdvancedEditor";
+import Home from "../component/Home";
 
 const RoutesController = () => {
   return (
@@ -8,6 +9,7 @@ const RoutesController = () => {
       <Routes>
         <Route path="/ck" element={<Ckeditor />} />
         <Route path="/advanced" element={<AdvancedEditor />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
